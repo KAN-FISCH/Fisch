ca46f36046781fc48054df4a024d09cb44eb6508
KAN-FISCH <shieldteamid@gmail.com>
2026-07-10 22:34:13 +0700

    Add files via upload

--- Modules/AntiAFK.lua
+++ Modules/AntiAFK.lua
@@ -1,15 +1,47 @@
--- AntiAFK.lua - Cegah kick AFK dengan mematikan koneksi Idled
+-- AntiAFK.lua - Cegah kick AFK dengan mematikan koneksi Idled dan simulasi input UNC
 local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
 
 local running = false
 local disabledConnections = {}
+local idledConnection = nil
+
+local function simulateInput()
+    -- Simulasikan input menggunakan fungsi UNC executor (bukan VirtualUser/VirtualInputManager)
+    pcall(function()
+        if keypress and keyrelease then
+            keypress(0x11) -- VK_CONTROL
+            task.wait(0.1)
+            keyrelease(0x11)
+        end
+    end)
+    pcall(function()
+        if mousemoveby then
+            mousemoveby(1, 1)
+            mousemoveby(-1, -1)
+        elseif mousemoverel then
+            mousemoverel(1, 1)
+            mousemoverel(-1, -1)
+        end
+    end)
+end
 
 local function start()
     if running then return end
+    
+    local LocalPlayer = Players.LocalPlayer
+    if not LocalPlayer then
+        task.spawn(function()
+            while not Players.LocalPlayer do
+                task.wait(0.5)
+            end
+            start()
+        end)
+        return
+    end
+
     running = true
 
-    -- Metode Utama: Mematikan koneksi event Idled (UNC/getconnections)
+    -- 1. Coba matikan koneksi default Idled
     if getconnections then
         pcall(function()
             for _, conn in pairs(getconnections(LocalPlayer.Idled)) do
@@ -22,13 +54,27 @@ local function start()
                 end
             end
         end)
-    else
-        warn("[NewFish5] Executor tidak mendukung 'getconnections'. Anti-AFK gagal diaktifkan.")
     end
+
+    -- 2. Hubungkan event Idled milik kita sendiri
+    -- Jika getconnections gagal atau tidak lengkap mematikan koneksi CoreScript,
+    -- event ini akan terpicu setelah 2 menit idle, lalu kita simulasikan input untuk reset timer.
+    pcall(function()
+        if idledConnection then
+            idledConnection:Disconnect()
+            idledConnection = nil
+        end
+        idledConnection = LocalPlayer.Idled:Connect(function()
+            if not running then return end
+            simulateInput()
+        end)
+    end)
 end
 
 local function stop()
     running = false
+    
+    -- Kembalikan koneksi default
     for _, conn in pairs(disabledConnections) do
         pcall(function()
             if type(conn) == "table" or typeof(conn) == "Connection" or type(conn) == "userdata" then
@@ -39,6 +85,12 @@ local function stop()
         end)
     end
     table.clear(disabledConnections)
+
+    -- Putuskan koneksi Idled kita
+    if idledConnection then
+        idledConnection:Disconnect()
+        idledConnection = nil
+    end
 end
 
 local AntiAFK = {}
@@ -54,5 +106,6 @@ setmetatable(AntiAFK, {
 })
 
 return AntiAFK
+
 
 
